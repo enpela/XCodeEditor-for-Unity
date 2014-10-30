f549fb09bf85a85654abd8751fdb79f5fe642137
Tetsuya MORI <monry84@gmail.com>
2014-10-30 13:48:37 +0900

    known region を削除するためのメソッドが無かったので追加

--- PBXProject.cs
+++ PBXProject.cs
@@ -35,5 +35,27 @@ namespace UnityEditor.XCodeEditor
 				_list.Add(region);
 			}
 		}
+
+		public void RemoveKnownRegion(string region)
+		{
+			if (string.IsNullOrEmpty(region)) {
+				return;
+			}
+
+			if (System.Text.RegularExpressions.Regex.IsMatch(region, "[a-z]{2}[_-][a-z]{2}", System.Text.RegularExpressions.RegexOptions.IgnoreCase)) {
+				region = "\"" + region.Replace("_", "-") + "\"";
+			}
+
+			PBXList _list = (PBXList)this.data["knownRegions"];
+			if (_list.Contains(region)) {
+				_list.Remove(region);
+			}
+		}
+
+		public void RemoveKnownRegionAll()
+		{
+			// やや強引だが、全部消しちゃうと動かなくなるので、初期値として英語を追加しておく
+			this.data["knownRegions"] = new PBXList("en");
+		}
 	}
 }
